76f0a10f32b8a2d0e50c92123f55775c2aeb01a1
ra-lgt <raviajay9344@gmail.com>
2025-11-02 21:00:40 +0530

    ui

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URI="https://1pmtsgzl-8080.inc1.devtunnels.ms/api/v1";
+export const API_BASE_URI="https://qs41n4rn-8080.inc1.devtunnels.ms/api/v1";
 export const WEB_CLIENT_ID="171687015795-9rmpn6dp4rj6ahjpktb6cr463455o858.apps.googleusercontent.com"
